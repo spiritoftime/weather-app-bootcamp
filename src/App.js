8a86894f9ae55fc9f19eb1f1eced9fabda43fd15
spiritoftime <chooliangyi6@gmail.com>
2023-02-11 07:30:33 +0800

    forked repo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
-
+import axios from "axios";
 class App extends React.Component {
   render() {
+    console.log(
+      axios.get(
+        "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=7c2fcdcce922e2e1173d4a9f734b7a05"
+      )
+    );
     return (
       <div className="App">
         <header className="App-header">
