d930c98a56e41d91544500b21718d3b751374f1c
spiritoftime <chooliangyi6@gmail.com>
2023-02-11 08:27:57 +0800

    made initial weather app

--- src/App.js
+++ src/App.js
@@ -3,19 +3,54 @@ import logo from "./logo.png";
 import "./App.css";
 import axios from "axios";
 class App extends React.Component {
-  render() {
-    console.log(
-      axios.get(
-        "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=7c2fcdcce922e2e1173d4a9f734b7a05"
+  constructor(props) {
+    super(props);
+    this.state = { inputValue: "", temperature: 0, fetched: false };
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+  }
+  clickHandler = (e) => {
+    e.preventDefault();
+    const res = axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${this.state.inputValue}&appid=7c2fcdcce922e2e1173d4a9f734b7a05`
       )
-    );
+      .then((res) => {
+        // console.log(res);
+        this.setState((prevState) => {
+          return {
+            inputValue: prevState.inputValue,
+            temperature: res.data.main.temp,
+            fetched: true,
+          };
+        });
+      });
+  };
+  onChangeHandler(e) {
+    this.setState({ inputValue: e.target.value, fetched: false });
+  }
+  render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <form onSubmit={this.clickHandler}>
+            <label>
+              Please type in a city to see its weather data:
+              <input
+                className="input"
+                value={this.state.inputValue}
+                onChange={this.onChangeHandler}
+              ></input>
+              <button type="submit">Submit</button>
+            </label>
+          </form>
+          {this.state.fetched ? (
+            <p>
+              the current temperature in {this.state.inputValue} is{" "}
+              {(this.state.temperature - 273).toFixed(1)} degree celsius.
+            </p>
+          ) : (
+            ""
+          )}
         </header>
       </div>
     );
